bae43c1726239e338842f80c323274ed9eaad231
Jakub Kumięga <jakub.kumiega@assecobs.pl>
2024-11-18 10:30:27 +0100

    add column in grid

--- src/app/interface/customers.ts
+++ src/app/interface/customers.ts
@@ -4,6 +4,24 @@ export interface Customer{
   street: string;
   city: string;
   postalCode: string;
+  country: string;
   customerVat: string;
+  accountNumber: string;
+  addressDetails: AddressDetails;
+  contactDetails: ContactDetails;
+}
 
+export interface AddressDetails {
+  name: number;
+  street: string;
+  city: string;
+  postalCode: string;
+  country: string;
+}
+
+export interface ContactDetails {
+  email: string;
+  phone: string;
+  website: string;
+  fax: string;
 }
